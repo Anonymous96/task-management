3b36b43afb7a9d49a01e8391d9d49f9155e8c884
Gari Iagutyan <gary.yagutyan@gmail.com>
2025-09-14 16:47:14 +0400

    added infrastructure

---
+++ libs/infrastructure/src/lib/infrastructure/infrastructure.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-infrastructure',
+  imports: [CommonModule],
+  templateUrl: './infrastructure.component.html',
+  styleUrl: './infrastructure.component.css',
+})
+export class InfrastructureComponent {}
